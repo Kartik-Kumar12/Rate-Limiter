bd04d6d4f927d27c47b04b22461a8978ab28f2dc
Kartikey <kartiksaurya05072001@gmail.com>
2024-10-14 01:28:38 +0530

    Added a Project Level Readme

--- rate_limiter_system/client/cmd/main.go
+++ rate_limiter_system/client/cmd/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/Kartik-Kumar12/Rate-Limiter/rate_limiter_system/client/services"
 	"github.com/Kartik-Kumar12/Rate-Limiter/rate_limiter_system/common/cli"
@@ -15,14 +13,14 @@ func main() {
 	cli.SetLogger()
 
 	// MAKE SEQUENTIAL REQUESTS
-	for i := 0; i < 30; i++ {
-		fmt.Printf("%vth iteration\n", i+1)
-		if err := services.ExecuteSequentially(); err != nil {
-			log.Error().Err(err).Msg("Failed to execute requests sequentially")
-			os.Exit(1)
-		}
-		time.Sleep(200 * time.Millisecond)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Printf("%vth iteration\n", i+1)
+	if err := services.ExecuteSequentially(); err != nil {
+		log.Error().Err(err).Msg("Failed to execute requests sequentially")
+		os.Exit(1)
 	}
+	// time.Sleep(200 * time.Millisecond)
+	// }
 
 	// MAKE PARALLEL REQUESTS
 	// for i := 0; i < 10; i++ {
